f3c3037a93c028d55c65e179da79ea4a7d1d1209
Yi Li <yl2326@cornell.edu>
2013-02-12 23:12:11 -0500

    added the project packages and some RSA stuff
    ^_^

---
+++ securedit_Main/src/RSA_Crypto/RSA_Crypto_Interface.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RSA_Crypto;
+
+/**
+ *
+ * @author Yi
+ */
+interface RSA_Crypto_Interface {
+    
+}
---
+++ securedit_Main/src/securedit_main/Securedit_Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package securedit_main;
+
+/**
+ *
+ * @author Yi
+ */
+public class Securedit_Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
