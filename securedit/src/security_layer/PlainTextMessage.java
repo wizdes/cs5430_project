e16d9ff0e537f74bba13fad55548feaf662c5cf2
Yi Li <yjli@USS-Prometheus.(none)>
2013-03-25 23:17:42 -0400

    fixed human authentication so it's actually legit

---
+++ securedit/src/security_layer/PlainTextMessage.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package security_layer;
+
+import application.encryption_demo.Message;
+
+/**
+ *
+ * @author yjli
+ */
+public class PlainTextMessage implements java.io.Serializable {
+    Message m;
+}
