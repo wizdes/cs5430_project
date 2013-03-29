b8fc5a2d78dbb03fce3c8d9cfaf3b5361c3bf8bb
Patrick Berens <pcb73@cornell.edu>
2013-03-29 12:55:28 -0600

    Missing configuration files and exception...my bad.

---
+++ securedit/src/configuration/Constants.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package configuration;
+
+/**
+ *
+ * @author Patrick
+ */
+public class Constants {
+    public static final boolean DEBUG_ON = true;
+    public static final String passwordCharacterRegex = ".*[^a-zA-Z\\s0-9].*";
+}
