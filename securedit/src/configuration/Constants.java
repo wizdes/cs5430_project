d46050c67955e3d69df0ca4f2473c2b40a238d30
Yi Li <yl2326@cornell.edu>
2013-03-30 01:23:07 -0400

    needs to be 12.
    I'm quoting a book:
    
    http://books.google.com/books?id=RtBvaW1HngYC&pg=PA28&lpg=PA28&dq=graphics+cards+password+security+georgia+tech&source=bl&ots=8-IMLPAFwG&sig=lUzsKpOO6HAAcAmt5hHZhk_BpRg&hl=en&sa=X&ei=cnVWUf3GI87E4AOYt4GACw&ved=0CDcQ6AEwATgK#v=onepage&q=graphics%20cards%20password%20security%20georgia%20tech&f=false

--- securedit/src/configuration/Constants.java
+++ securedit/src/configuration/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public static final boolean DEBUG_ON = true;
     public static final String passwordCharacterRegex = ".*[^a-zA-Z\\s0-9].*";
     
-    public static final int PIN_LENGTH = 8;
+    public static final int PIN_LENGTH = 12;
     public static final String GLOBAL_ENCODING = "UTF-8";
     
     //Ascii without alphanumeric, spaces, and junk stuff don't want.
