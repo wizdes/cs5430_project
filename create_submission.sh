5dd178596185b60fe73c12bd55e4c21c229fd037
Matt Goggin <goggin13@gmail.com>
2013-03-30 12:03:19 -0400

    update submission script

--- create_submission.sh
+++ create_submission.sh
@@ -16,4 +16,4 @@ rm -rf submission/src/_old_stuff
 cp cs5430_project/securedit/dist/securedit.jar submission/
 cp -r cs5430_project/securedit/dist/lib submission/
 
-#tar -cvzf submission.tar.gz submission
+zip -r submission submission
